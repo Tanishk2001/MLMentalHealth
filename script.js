db9929136ec61a16899c16434d6f4087814fe170
Tanishk Agrawal <tanishk15112001@gmail.com>
2026-08-16 20:29:08 +0530

    Updated js file with deployed URL

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (() => {
-  const API_BASE = 'http://127.0.0.1:2200';
+  const API_BASE = "https://mlmentalhealth.onrender.com";
 
   /* ---------------------------------------------------------
      Element refs
